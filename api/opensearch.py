ff0f4e5c8275901f2f151488260d91956fcd13ff
Vaibhav Mahajan <vamahaja@redhat.com>
2025-07-24 12:56:22 +0530

    [LibFix] Add support to update opensearch with teuthology job logs
    Signed-off-by: Vaibhav Mahajan <vamahaja@redhat.com>

--- api/opensearch.py
+++ api/opensearch.py
@@ -3,10 +3,12 @@ Update job details to opensearch
 
 Usage:
     opensearch.py --config=<cfg-file> --testruns-dir=<dir>
+        [--skip-logs]
 
 Options:
     --config=<cfg-file>     Path to the configuration file.
     --testruns-dir=<dir>    Path to the test runs directory.
+    --skip-logs             Skip job logs.
 """
 
 import json
@@ -14,7 +16,7 @@ import os
 from datetime import datetime
 
 from docopt import docopt
-from opensearchpy import OpenSearch
+from opensearchpy import OpenSearch, helpers
 from utils import get_config
 
 INDEX_CONFIG = {
@@ -29,6 +31,15 @@ INDEX_CONFIG = {
     },
     "jobs": {
         "name": "teuthology-jobs",
+        "body": {
+            "settings": {
+                "index.mapping.total_fields.limit": 10000,
+                "index.mapping.ignore_malformed": True,
+            },
+        },
+    },
+    "logs": {
+        "name": "teuthology-logs",
         "body": {
             "settings": {
                 "index.mapping.total_fields.limit": 10000,
@@ -103,7 +114,7 @@ def read_metadata(file_name):
         return json.load(_f)
 
 
-def insert_jobs(client, runs, testrun_path):
+def insert_jobs(client, runs, testrun_path, skip_logs):
     """Insert teuthology jobs in Opensearch"""
     for job_id in runs.get("job_ids", []):
         print(f"Processing job id: {job_id}")
@@ -117,8 +128,83 @@ def insert_jobs(client, runs, testrun_path):
         _index = INDEX_CONFIG.get("jobs").get("name")
         try:
             insert_record(client, _index, job_id, job_data)
-        except Exception as e:
-            print(f"Error: Failed to insert job {job_id}")
+        except Exception:
+            print(f"Error: Failed to insert job job-id {job_id}")
+
+        if skip_logs:
+            continue
+
+        # Get log file path
+        log_file = f"{os.path.join(testrun_path, 'logs', job_id)}.log"
+
+        # Update job logs
+        _index = INDEX_CONFIG.get("logs").get("name")
+        try:
+            insert_logs(client, _index, job_id, log_file)
+        except Exception:
+            print(f"Error: Failed to insert logs for job-id {job_id}")
+
+
+def insert_logs(client, index, id, log_file):
+    """Insert logs into OpenSearch"""
+    print(f"Indexing logs in OpenSearch: {index}/{id}")
+    batch_size, batch_index = 1000, 0
+
+    # Iterate over log file for given batch size
+    def batchify(iterable, batch_size=batch_size):
+        batch = []
+        for item in iterable:
+            batch.append(item)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        if batch:
+            yield batch
+
+    # Open log file and start update logs
+    with open(log_file, "r", encoding="utf-8") as f:
+        # Iterate over log file lines
+        lines = (line for line in f if line.strip())
+
+        for batch_lines in batchify(lines, batch_size):
+            print(f"Processing batch {batch_index + 1}")
+
+            # Set action for job-id
+            actions = [
+                {
+                    "_index": index,
+                    "_source": {"message": line.rstrip("\n"), "job-id": id},
+                }
+                for line in batch_lines
+            ]
+
+            # Update logs to index
+            try:
+                helpers.bulk(client, actions)
+            except Exception as e:
+                print(f"Error during ingestion: {e}")
+
+            batch_index += 1
+
+
+def update_runs(client, testruns_dir, skip_logs):
+    """Update teuthology runs in OpenSearch"""
+    for testruns in os.scandir(testruns_dir):
+        # Check for directory
+        if not testruns.is_dir():
+            continue
+
+        print(f"Processing testruns: {testruns.name}")
+
+        # Get job ids from meatadata
+        runs = read_metadata(os.path.join(testruns.path, "results.json"))
+
+        # Insert jobs to opensearch
+        insert_jobs(client, runs, testruns.path, skip_logs)
+
+        # Insert runs to openserach
+        _index = INDEX_CONFIG.get("runs").get("name")
+        insert_record(client, _index, runs.get("name"), runs)
 
 
 def insert_record(client, index, id, body):
@@ -138,27 +224,7 @@ def insert_record(client, index, id, body):
         raise ValueError(f"Failed to index documents. Response:\n{response}")
 
 
-def update_runs(client, testruns_dir):
-    """Update teuthology runs in OpenSearch"""
-    for testruns in os.scandir(testruns_dir):
-        # Check for directory
-        if not testruns.is_dir():
-            continue
-
-        print(f"Processing testruns: {testruns.name}")
-
-        # Get job ids from meatadata
-        runs = read_metadata(os.path.join(testruns.path, "results.json"))
-
-        # Insert jobs to opensearch
-        insert_jobs(client, runs, testruns.path)
-
-        # Insert runs to openserach
-        _index = INDEX_CONFIG.get("runs").get("name")
-        insert_record(client, _index, runs.get("name"), runs)
-
-
-def main(config, testruns_dir):
+def main(config, testruns_dir, skip_logs):
     # Connect to OpenSearch
     client = connect(config)
 
@@ -171,7 +237,7 @@ def main(config, testruns_dir):
         create_index(client, index.get("name"), index.get("body"))
 
     # Update teuthology runs
-    update_runs(client, testruns_dir)
+    update_runs(client, testruns_dir, skip_logs)
 
 
 if __name__ == "__main__":
@@ -189,5 +255,8 @@ if __name__ == "__main__":
     config = args["--config"]
     testruns_dir = args["--testruns-dir"]
 
+    # Get log flag
+    skip_logs = args["--skip-logs"]
+
     # Update OpenSearch data
-    main(config, testruns_dir)
+    main(config, testruns_dir, skip_logs)
